d2b0490f3ef2ca24d6a0c70c816ed48f0ce5caa9
Samuel Aliyari <samuelaliyari@outlook.de>
2023-11-20 16:04:26 +0100

    Fetch function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-console.log("test")
+
+const cityName = "London"
+
+const getLat = () => {
+    const api = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=8ff0dd8b9a61cd670eec3ca9cba7e8f2`
+    fetch(api)
+    .then(response => response.json())
+    .then(data => {
+        const lat = (data[0].lat.toFixed(2))
+        const lon = (data[0].lon.toFixed(2))
+        const api = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=8ff0dd8b9a61cd670eec3ca9cba7e8f2`
+        fetch(api)
+        .then(answer => answer.json())
+        .then(cityData => {
+            const dataStorage = {...cityData}
+            console.log(dataStorage)
+        })
+    })
+}
+
+getLat()
+
+
+
+
