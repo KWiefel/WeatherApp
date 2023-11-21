3aca17bb0e5d303b0c7e73cb682f37acabd0610b
Samuel Aliyari <samuelaliyari@outlook.de>
2023-11-21 14:38:48 +0100

    Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ const getLatLon = () => {
             .then(cityData => {
                 const lat = (cityData[0].lat.toFixed(2))
                 const lon = (cityData[0].lon.toFixed(2))
-                const api = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=8ff0dd8b9a61cd670eec3ca9cba7e8f2`
+                const api = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=8ff0dd8b9a61cd670eec3ca9cba7e8f2`
                 fetch(api)
                 .then(response => response.json())
                 .then(weatherData => {
